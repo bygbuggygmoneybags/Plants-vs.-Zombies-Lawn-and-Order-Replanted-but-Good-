859926cde9d34b0bfe682a0b6d00d526a39f8f6d
FinalAquaticZenith <auswoods2006@gmail.com>
2025-12-04 21:37:17 -0700

    fixed sun.gd

--- pvz-lawn-and-order/GenMechanics/PlantTiles.cs
+++ pvz-lawn-and-order/GenMechanics/PlantTiles.cs
@@ -3,43 +3,43 @@ using System;
 
 namespace pvzlawnandorder
 {
-    public partial class PlantTiles : PlantLayer
-    {
-        [Export] private int tileSourceId = 0;
-        [Export] private Vector2I tileAtlasCoords = new Vector2I(0,0);
-        [Export] private int gridWidth = 9;
-        [Export] private int gridHeight = 5;
+	public partial class PlantTiles
+	{
+		[Export] private int tileSourceId = 0;
+		[Export] private Vector2I tileAtlasCoords = new Vector2I(0,0);
+		[Export] private int gridWidth = 9;
+		[Export] private int gridHeight = 5;
 
-        [Export] private TileSet tileSetResource = "res://TileSet.tres";
+		[Export] private TileSet tileSetResource = "res://TileSet.tres";
 
 
 
-        public override void _Ready()
-        {
-            if (TileSet == null)
-            {
-                if (tileSetResource != null)
-                {
-                    TileSet = tileSetResource;
-                } else 
-                {
-                    GD.PrintErr("No Tile set assigned to TileMapLayer");
-                }
-            }
+		public override void _Ready()
+		{
+			if (TileSet == null)
+			{
+				if (tileSetResource != null)
+				{
+					TileSet = tileSetResource;
+				} else 
+				{
+					GD.PrintErr("No Tile set assigned to TileMapLayer");
+				}
+			}
 
-            for (int x = 0; x < gridWidth; x++)
-            {
-                for (int y = 0; y < gridHeight; y++)
-                {
-                    Vector2I cellCoords = new Vector2I(x, y);
-                    SetCell(cellCoords, tileSourceId, tileAtlasCoords);
-                    TileData tileData = GetCellTileData(coords);
-                    if (tileData != null)
-                    {
-                        tileData.SetCustomData("plantable", true);
-                    }
-                }
-            }
-        }
-    }
+			for (int x = 0; x < gridWidth; x++)
+			{
+				for (int y = 0; y < gridHeight; y++)
+				{
+					Vector2I cellCoords = new Vector2I(x, y);
+					SetCell(cellCoords, tileSourceId, tileAtlasCoords);
+					TileData tileData = GetCellTileData(coords);
+					if (tileData != null)
+					{
+						tileData.SetCustomData("plantable", true);
+					}
+				}
+			}
+		}
+	}
 }
--- pvz-lawn-and-order/sun.gd
+++ pvz-lawn-and-order/sun.gd
@@ -3,6 +3,7 @@ extends Area2D
 var end_y
 var zf
 var vel = Vector2(0.3, -6)
+var score = 0
 @onready var tween = create_tween()
 
 
@@ -11,12 +12,10 @@ func _ready():
 	pass
 
 
-# 从天空中落下的阳光
 func init_sky():
 	global_position.x = randi_range(200, 1000)
 	global_position.y = -20
 	end_y = randi_range(100, 550)
-	# 创建下落动画
 	self.tween.tween_property(self, "global_position:y", end_y, end_y / 100)
 
 
@@ -43,20 +42,16 @@ func _process(_delta):
 
 
 func collected_success():
-	PlantsBarAutoload.sun += PlantsBarAutoload.sun_add_once
+	score+=25
 	queue_free()
 
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton && event.pressed:
-		# 移动到计数牌，删除自身
 		self.tween.kill()
 		self.tween = create_tween()
-		var stop = $"/root/main/score".global_position
-		var time = (stop - self.global_position).length() / 500
-		self.tween.tween_property(self, "global_position", stop, time)
 		self.tween.tween_callback(self.collected_success)
-		$disappeartimer.stop()
+
 
 
 func _on_disappeartimer_timeout():
