1190d3515dd1d634e17484b5f1a55bbc0d19c23b
FinalAquaticZenith <auswoods2006@gmail.com>
2025-12-08 09:38:25 -0700

    Implemented options menu and fullscreen

--- pvz-lawn-and-order/menu.gd
+++ pvz-lawn-and-order/menu.gd
@@ -1,12 +1,17 @@
 extends Node2D
-@onready var account = get_node("Camera2D/AccountButton")
-@onready var adventure = get_node("Camera2D/AdventureButton")
-@onready var minigame = get_node("Camera2D/Mini-GameButton")
-@onready var puzzle = get_node("Camera2D/PuzzleButton")
-@onready var survival = get_node("Camera2D/SurvivalButton")
-@onready var options = get_node("Camera2D/OptionsButton")
-@onready var error = get_node("Camera2D/error")
-@onready var optionMenu = get_node("Camera2D/MenuBar")
+@onready var account = get_node("MainMenu/AccountButton")
+@onready var adventure = get_node("MainMenu/AdventureButton")
+@onready var minigame = get_node("MainMenu/Mini-GameButton")
+@onready var puzzle = get_node("MainMenu/PuzzleButton")
+@onready var survival = get_node("MainMenu/SurvivalButton")
+@onready var options = get_node("MainMenu/OptionsButton")
+@onready var error = get_node("MainMenu/error")
+@onready var optionMenu = get_node("MainMenu/GridContainer/MenuBar")
+@onready var back = get_node("MainMenu/GridContainer/MenuBar/Back")
+@onready var music = get_node("MainMenu/GridContainer/MenuBar/Music")
+@onready var sound = get_node("MainMenu/GridContainer/MenuBar/Sound")
+@onready var accel = get_node("MainMenu/GridContainer/MenuBar/3d Acceleration")
+@onready var fullscreen = get_node("MainMenu/GridContainer/MenuBar/Fullscreen")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +22,13 @@ func _ready() -> void:
 	puzzle.pressed.connect(puzzle_pressed)
 	survival.pressed.connect(survival_pressed)
 	options.pressed.connect(options_pressed)
+	back.pressed.connect(back_pressed)
+	music.changed.connect(music_slider)
+	sound.changed.connect(sound_slider)
+	accel.pressed.connect(accel_checked)
+	fullscreen.pressed.connect(fullscreen_checked)
+	fullscreen.pressed.connect(fullscreen_unchecked)
+	optionMenu.set("visible",false)
 
 func account_pressed():
 	error_popup()
@@ -37,14 +49,25 @@ func puzzle_pressed():
 func survival_pressed():
 	error_popup()
 
-func options_pressed():
-	optionMenu.set("visiblity", true)
+func options_pressed() -> void:
+	optionMenu.set("visible", true)
 
 
 func error_popup():
 	error.popup()
-	#error.set_text("This isn't implemented. Sorry for the inconvience. :(")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+
+func back_pressed():
+	set("visible",false)
+	
+func music_slider():
 	pass
+func sound_slider():
+	pass
+func accel_checked():
+	pass
+func fullscreen_checked():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	pass
+func fullscreen_unchecked():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
